db2cdd8f974c690397b789b154da7a9fbec10ff8
morrisjh <43405063+morrisjh@users.noreply.github.com>
2019-07-01 18:11:40 +0900

    Add files via upload

---
+++ info.py
@@ -0,0 +1,10 @@
+""" BasicTextAnalyzer Information """
+
+
+__author__ = "James Morris"
+__maintainer__ = "James Morris"
+__email__ = "morrisjamesharry@gmail.com"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__credits__ = ["Tyler Barrus and Peter Norvig (for pyspellchecker"]
+__url__ = "https://github.com/morrisjh/Basic-Text-Analyzer"
